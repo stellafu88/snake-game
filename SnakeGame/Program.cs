a210f3bb60b96c1cffafc4f4376cf037c3d49393
stella <stella@JYs-MacBook-Air.local>
2025-02-27 22:52:26 +1100

    Print canva

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program {
+    static string[][] canva = new string[][] {
+        new string[] {".", ".", "."},
+        new string[] {".", ".", "."},
+        new string[] {".", ".", "."}
+    };
+
+    public static void Print() {
+        for (int i = 0; i < canva.Length; i++){
+            Console.WriteLine(string.Join(" ", canva[i]));
+        }
+    }
+
+    public static void Main() {
+        Print();
+    }
+}
+
